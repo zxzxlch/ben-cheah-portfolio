2cf3099478c32aaa17e119337a86a6f82cd1ef6b
Ben C <zxzxlch@gmail.com>
2022-03-12 18:04:47 +0800

    Fix container centering

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -7,12 +7,12 @@ import AnimatedGradient from './animated-gradient';
 const Layout = ({ children }) => {
   return (
     <>
-      <div className="container text-base">
+      <div className="text-base">
         <Helmet defaultTitle="Ben Cheah &middot; UX Designer" titleTemplate="Ben Cheah &middot; %s">
           <html lang="en" />
           <meta charSet="utf-8" />
         </Helmet>
-        <div className="mx-auto max-w-3xl">
+        <div className="mx-auto container max-w-3xl">
           <Navbar />
           <main className="px-5 sm:px-0 grid grid-cols-4 sm:grid-cols-8 gap-4 antialiased">
             {children}
