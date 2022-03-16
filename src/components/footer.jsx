433988b173c7e66016393c4c559e6a2b049150c2
Ben C <zxzxlch@gmail.com>
2022-03-17 00:49:14 +0800

    Add Github link to footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { Github as GithubIcon } from '@styled-icons/fa-brands';
 
 const Footer = () => {
   const menuLinks = [
@@ -18,7 +19,19 @@ const Footer = () => {
   ));
   return (
     <footer className="mx-3 mt-20 px-2 pt-8 pb-6 sm:mx-0 sm:px-0 border-t border-gray-600 text-sm">
-      <ul className="flex space-x-12">{menuLinks}</ul>
+      <ul className="flex space-x-12">
+        {menuLinks}
+        <li className="flex grow justify-end">
+          <a
+            href="https://github.com/zxzxlch/ben-cheah-portfolio"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <GithubIcon className="-mt-2 text-gray-700" size="24" />
+            <span className="sr-only">Github repository</span>
+          </a>
+        </li>
+      </ul>
       <div className="mt-12 text-xs text-gray-600">&#169;2022 Ben Cheah</div>
     </footer>
   );
