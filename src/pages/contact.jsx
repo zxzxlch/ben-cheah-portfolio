80a520ec33801942d8a6e1fd1e852661af3e9d3d
Ben C <zxzxlch@gmail.com>
2022-04-01 14:54:10 +0800

    Remove email obfuscation

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -14,11 +14,9 @@ const Index = () => {
         <div id="main" className={post}>
           <PageTitle>Contact</PageTitle>
           <p>
-            Hey there! If you'd like to reach out, you can{' '}
-            <a href="mail&#116;o&#58;&#37;62e&#37;&#54;&#69;&#37;&#54;&#65;&#99;%68%62&#64;&#103;m&#97;il%2Ec&#37;6Fm">
-              drop me an email
-            </a>{' '}
-            or find me on{' '}
+            Hey there! If you'd like to reach out, you can email me at{' '}
+            <a href="mailto:benjchb@gmail.com">benjchb@gmail.com</a> or find me
+            on{' '}
             <a
               href="https://www.linkedin.com/in/benjamincheah/"
               target="_blank"
